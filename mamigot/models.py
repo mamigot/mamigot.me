6496fe84687779f75d446a50af32d9c218f559b9
miguel5 <miguel.amigot@gmail.com>
2014-12-30 21:52:02 -0500

    Post model

--- mamigot/models.py
+++ mamigot/models.py
@@ -1,1 +1,12 @@
-# Models for main app
+import datetime
+from mamigot import db
+
+
+class Post(db.Document):
+    created_at  = db.DateTimeField(default=datetime.datetime.now, required=True)
+    modified_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+
+    title = db.StringField(max_length=255, required=True)
+    slug  = db.StringField(max_length=255, required=True)
+    desc  = db.StringField(required=True)
+    body  = db.StringField(required=True)
