aecf038e5ecad84c7b671718de75023af4bf680b
miguel5 <miguel.amigot@gmail.com>
2015-01-02 01:02:29 -0500

    API endpoints for projects and images

--- mamigot/api/urls.py
+++ mamigot/api/urls.py
@@ -1,6 +1,6 @@
 from mamigot.api import app
+from views.own import BlogPostAPI, ProjectPostAPI, ImageAPI
 from views.ext import linkedin, github
-from views.own import BlogPostAPI
 
 
 app.add_url_rule('/linkedin/<string:item>', view_func = linkedin)
@@ -14,3 +14,17 @@ blog_post_view = BlogPostAPI.as_view('blog_post_api')
 app.add_url_rule('/blog/posts/', view_func = blog_post_view)
 
 app.add_url_rule('/blog/posts/<string:slug>', view_func = blog_post_view)
+
+
+project_post_view = ProjectPostAPI.as_view('project_post_api')
+
+app.add_url_rule('/projects/posts/', view_func = project_post_view)
+
+app.add_url_rule('/projects/posts/<string:slug>', view_func = project_post_view)
+
+
+image_view = ImageAPI.as_view('image_api')
+
+app.add_url_rule('/images/', view_func = image_view)
+
+app.add_url_rule('/images/<string:slug>', view_func = image_view)
