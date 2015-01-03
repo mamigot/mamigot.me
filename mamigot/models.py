49b83746545b63106086e69356c98123319926cb
miguel5 <miguel.amigot@gmail.com>
2015-01-02 20:06:48 -0500

    Require dates, and method to list necessary, user-inputted fields

--- mamigot/models.py
+++ mamigot/models.py
@@ -1,10 +1,11 @@
+from mongoengine.fields import DateTimeField, ObjectIdField
 import datetime
 from mamigot import db
 
 
 class Post(object):
-    created_at  = db.DateTimeField(default=datetime.datetime.now)
-    modified_at = db.DateTimeField(default=datetime.datetime.now)
+    created_at  = db.DateTimeField(default=datetime.datetime.now, required=True)
+    modified_at = db.DateTimeField(default=datetime.datetime.now, required=True)
 
     title = db.StringField(max_length=255, required=True)
     slug  = db.StringField(max_length=255, required=True)
@@ -19,6 +20,16 @@ class Post(object):
     def get_required_fields(cls):
         if issubclass(cls, db.Document):
             return [k for k,v in cls._fields.iteritems() if v.required]
+
+        else: raise NotImplementedError
+
+    @classmethod
+    def get_manual_fields(cls):
+        if issubclass(cls, db.Document):
+            excluded_types = [DateTimeField, ObjectIdField]
+
+            return [k for k,v in cls._fields.iteritems()
+                    if type(v) not in excluded_types]
 
         else: raise NotImplementedError
 
