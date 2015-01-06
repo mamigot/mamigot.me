cc6b397856c44f921f2840e5ef55fc5616575017
miguel5 <miguel.amigot@gmail.com>
2015-01-06 00:36:32 -0500

    Renamed app (see why below) and used assets
    At “About the First Parameter”
    http://flask.pocoo.org/docs/0.10/api/#flask.Flask

--- mamigot/api/__init__.py
+++ mamigot/api/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 import settings as config_vars # Custom config vars
 
 
-app = Flask('api')
+app = Flask('mamigot.api')
 app.config.from_object(config_vars)
 
 
--- mamigot/frontend/__init__.py
+++ mamigot/frontend/__init__.py
@@ -2,5 +2,13 @@ from flask import Flask
 import settings as config_vars # Custom config vars
 
 
-app = Flask('frontend')
+app = Flask('mamigot.frontend')
 app.config.from_object(config_vars)
+
+
+import urls # After app is created, add url_rules
+
+
+# Configure assets
+from assets import init_app
+init_app(app)
