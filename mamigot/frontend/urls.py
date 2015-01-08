426c12b6b2dd220ee9a360d53dc88d2f8140182e
miguel5 <miguel.amigot@gmail.com>
2015-01-08 18:46:19 -0500

    Frontend URLs

--- mamigot/frontend/urls.py
+++ mamigot/frontend/urls.py
@@ -1,17 +1,25 @@
-from mamigot import app
+from mamigot.frontend import app
+from views import index, blog_archive, blog_post, \
+                  projects, project_post, resume_html, resume_pdf, \
+                  ext_profile
 
 
-app.add_url_rule('/', view_func = main_page)
+url_date = '<int:year>/<int:month>/<int:day>'
 
 
-app.add_url_rule('/resume/', view_func = resume_html)
-app.add_url_rule('/resume/pdf/' + url_date, view_func = resume_pdf)
+app.add_url_rule('/', view_func = index)
 
 
-app.add_url_rule('/projects', view_func = projects_archive)
+app.add_url_rule('/blog/', view_func = blog_archive)
+app.add_url_rule('/blog/' + url_date + '/<string:slug>', view_func = blog_post)
+
+
+app.add_url_rule('/projects/', view_func = projects)
 app.add_url_rule('/projects/<string:slug>', view_func = project_post)
 
 
-app.add_url_rule('/blog', view_func = blog_archive)
-app.add_url_rule('/blog/<int:year>/<int:month>/<int:day>/<string:slug>',
-                 view_func = blog_post)
+app.add_url_rule('/resume/', view_func = resume_html)
+app.add_url_rule('/resume/pdf/', view_func = resume_pdf)
+
+
+app.add_url_rule('/profile/<string:site>/', view_func = ext_profile)
