bad4a1ec83bdeacd192959bc942e3f7d1862ec43
miguel5 <miguel.amigot@gmail.com>
2015-01-09 01:31:08 -0500

    Allow multiple processes
    (Required to complete front-end requests while waiting for the API to
    work; with a single process, there’s deadlock)

--- wsgi.py
+++ wsgi.py
@@ -13,5 +13,6 @@ application = DispatcherMiddleware(frontend, {
 
 
 if __name__ == "__main__":
+
     run_simple( '0.0.0.0', 5000, application,
-                use_reloader=True, use_debugger=True)
+                use_reloader=True, use_debugger=True, threaded=True)
