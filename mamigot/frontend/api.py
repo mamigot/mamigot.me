23d05b36d41290b25da7d955bd5911be6241a81a
miguel5 <miguel.amigot@gmail.com>
2015-01-09 23:41:45 -0500

    Function to get a single post from the API + refactoring

--- mamigot/frontend/api.py
+++ mamigot/frontend/api.py
@@ -12,13 +12,13 @@ def format_date(date_str, input_format='%Y-%m-%d %H:%M:%S.%f'):
     return dt_obj.strftime(output_format)
 
 
-def issue_get_request(post_type, url_params):
+def issue_get_request(relative_url):
 
     @copy_current_request_context
     def get_base_url():
-        return 'http://' + request.host + "/api/"
+        return 'http://' + request.host
 
-    full_url = get_base_url() + post_type + "/posts" + url_params
+    full_url = get_base_url() + relative_url
     return requests.get(full_url)
 
 
@@ -27,8 +27,8 @@ def get_posts_list(post_type, limit="", addl_url_params=None):
     addl_url_params is a dictionary
     '''
     post_type = post_type.lower()
-    if post_type not in ['blog', 'projects']:
-        raise NotImplementedError('Given post_type is not implemented.')
+    if post_type != 'blog' and post_type != 'projects':
+        raise NotImplementedError
 
     # All we need for a list representation
     relevant_fields = ['title', 'slug', 'desc', 'created_at']
@@ -45,10 +45,34 @@ def get_posts_list(post_type, limit="", addl_url_params=None):
 
     stringified_params = [ (k + "=" + v) for k,v in url_params.iteritems() ]
     url_vars = "?" + "&".join(stringified_params)
+    relative_url = "/api/" + post_type + "/posts" + url_vars
 
-    content = issue_get_request(post_type, url_vars).json()
-    # Format dates
-    for post in content:
+    r = issue_get_request(relative_url)
+    js_content = r.json()
+
+    # Format date (we know that the only fetched date is "created_at")
+    for post in js_content:
         post["created_at"] = format_date( post["created_at"] )
 
-    return content[::-1] # Show most recent first
+    return js_content[::-1] # Show most recent first
+
+
+def get_single_post(post_type, slug):
+    post_type = post_type.lower()
+    if post_type != 'blog' and post_type != 'projects':
+        raise NotImplementedError
+
+    relative_url = "/api/" + post_type + "/posts/" + slug
+
+    r = issue_get_request(relative_url)
+    if r.status_code is not 200:
+        raise r.raise_for_status()
+
+    js_content = r.json()
+
+    # Format dates
+    for post in js_content:
+        post["created_at"] = format_date( post["created_at"] )
+        post["modified_at"] = format_date( post["modified_at"] )
+
+    return js_content
