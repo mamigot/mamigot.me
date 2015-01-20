68b954a61f9f20e9dc1a35b09a18f6b1e006f58c
miguel5 <miguel.amigot@gmail.com>
2015-01-20 14:20:01 -0500

    Added closure and defined PostDetailController

--- mamigot/frontend/static/app/components/blog/blog.js
+++ mamigot/frontend/static/app/components/blog/blog.js
@@ -1,20 +1,21 @@
-var app = angular.module('blog', [
-  'ngRoute',
-]);
+(function(){
 
+  var app = angular.module('blog', ['ngRoute']);
 
-app.config(['$routeProvider', '$locationProvider',
+  app.config(['$routeProvider', '$locationProvider',
 
-  function($routeProvider, $locationProvider){
-    $routeProvider
+    function($routeProvider, $locationProvider){
+      $routeProvider
 
-    .when('/blog', {
-      templateUrl: 'static/app/partials/blog-archive.html',
-      // pass controller here
-    })
+      .when('/blog', {
+        templateUrl: 'static/app/partials/blog-archive.html',
+      })
 
-    .when('/blog/:year/:month/:day/:slug', {
-      templateUrl: 'static/app/partials/blog-archive.html',
-    })
+      .when('/blog/:slug', {
+        templateUrl: 'static/app/partials/_post-detail.html',
+        controller: 'PostDetailController',
+      })
 
-  }]);
+    }]);
+
+})();
---
+++ mamigot/frontend/static/app/components/blog/controllers/PostDetailController.js
@@ -0,0 +1,17 @@
+angular.module('blog')
+
+  .controller('PostDetailController', ['$scope', '$routeParams', '$http',
+    function($scope, $routeParams, $http){
+
+      var slug = $routeParams.slug;
+
+
+      $http.get('/api/blog/posts/' + slug).success(function(data){
+        // Body HTML is automatically sanitized using ng-sanitize
+        // https://docs.angularjs.org/api/ngSanitize/service/$sanitize
+        
+        $scope.post = data[0];
+
+      });
+
+  }]);
