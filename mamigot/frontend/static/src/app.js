295cf2d941041d7819c0384ef4feb1525dbb246e
miguel5 <miguel.amigot@gmail.com>
2015-01-21 00:24:29 -0500

    Encapsulate files to limit risk of name collision when concatenating

--- mamigot/frontend/static/src/app.js
+++ mamigot/frontend/static/src/app.js
@@ -1,24 +1,28 @@
-var app = angular.module('main', ['ngRoute', 'ngSanitize', 'blog', 'shared']);
+(function(){
+
+  var app = angular.module('main', ['ngRoute', 'ngSanitize', 'blog', 'shared']);
 
 
-app.config(['$routeProvider', '$locationProvider',
+  app.config(['$routeProvider', '$locationProvider',
 
-  function($routeProvider, $locationProvider){
-    $routeProvider
+    function($routeProvider, $locationProvider){
+      $routeProvider
 
-    .when('/', {
-      templateUrl: 'static/src/partials/home.html',
-    })
+        .when('/', {
+          templateUrl: 'static/src/partials/home.html',
+        })
 
-    .otherwise({
-      templateUrl: 'static/src/partials/errors/404.html',
-    });
+        .otherwise({
+          templateUrl: 'static/src/partials/errors/404.html',
+        });
 
-    // https://docs.angularjs.org/error/$location/nobase
-    $locationProvider.html5Mode({
-      enabled: true
-    });
-    // https://github.com/angular/angular.js/issues/5519
-    $locationProvider.hashPrefix('!');
+      // https://docs.angularjs.org/error/$location/nobase
+      $locationProvider.html5Mode({
+        enabled: true
+      });
+      // https://github.com/angular/angular.js/issues/5519
+      $locationProvider.hashPrefix('!');
 
-  }]);
+    }]);
+
+})();
--- mamigot/frontend/static/src/components/shared/shared.js
+++ mamigot/frontend/static/src/components/shared/shared.js
@@ -1,1 +1,5 @@
-var app = angular.module('shared', []);
+(function(){
+
+  var app = angular.module('shared', []);
+
+})();
