5d0beaa069de4b3a3cca128ad8f6f2dd5068232d
miguel5 <miguel.amigot@gmail.com>
2015-01-25 10:04:54 -0500

    Possibility to limit the number of blog posts
    The <blog-list> directive accepts a ‘limit’ attribute e.g. <blog-list
    limit=3> that saves it onto the scope of the template it calls (thus
    allowing an e.g. controller that uses that scope to access it to limit
    its API call).

--- mamigot/frontend/static/src/components/blog/controllers/BlogListCtrl.js
+++ mamigot/frontend/static/src/components/blog/controllers/BlogListCtrl.js
@@ -1,12 +1,13 @@
 angular.module('blog')
 
-.controller('BlogListCtrl', ['$http',
-  function($http){
+.controller('BlogListCtrl', ['$http', '$scope', function($http, $scope){
 
     var c = this;
     c.posts = {};
 
-    $http.get('/api/blog/posts?fields=title,created_at')
+    var limitParam = $scope.limit ? "&limit=" + $scope.limit : "";
+
+    $http.get('/api/blog/posts?fields=title,created_at' + limitParam)
       .success(function(data){
 
         c.posts = data;
--- mamigot/frontend/static/src/components/blog/directives/blogList.js
+++ mamigot/frontend/static/src/components/blog/directives/blogList.js
@@ -1,10 +1,31 @@
 angular.module('blog')
 
+  /*
+  Retrieves a list of the posts in the blog through the template below. Though
+  the retrieval is up to the controller called in the template, this directive
+  sets a 'limit' variable on the template's scope that the controller can use
+  to set a limit for the number of posts it retrieves.
+
+  EXAMPLE:
+    No limit:
+      <blog-list></blog-list>
+
+    Set a limit to 3:
+      <blog-list limit=3></blog-list>
+  */
   .directive('blogList', function(){
 
     return {
       restrict: 'E',
       templateUrl: 'static/src/partials/_blog-list.html',
+
+      controller: function($scope, $attrs){
+        /*
+        Set 'limit' on the template's scope according to the attribute
+        on the directive. This only affects the template's scope.
+        */
+        $scope.limit = $attrs.limit;
+      }
     };
 
   });
