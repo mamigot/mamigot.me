f8f2682713df5df3b019ba03d9dfa4f448a7f016
miguel5 <miguel.amigot@gmail.com>
2015-01-25 10:06:39 -0500

    Formatting

--- mamigot/frontend/static/src/components/blog/controllers/BlogListCtrl.js
+++ mamigot/frontend/static/src/components/blog/controllers/BlogListCtrl.js
@@ -1,17 +1,17 @@
 angular.module('blog')
 
-.controller('BlogListCtrl', ['$http', '$scope', function($http, $scope){
+  .controller('BlogListCtrl', ['$http', '$scope', function($http, $scope){
 
-    var c = this;
-    c.posts = {};
+      var c = this;
+      c.posts = {};
 
-    var limitParam = $scope.limit ? "&limit=" + $scope.limit : "";
+      var limitParam = $scope.limit ? "&limit=" + $scope.limit : "";
 
-    $http.get('/api/blog/posts?fields=title,created_at' + limitParam)
-      .success(function(data){
+      $http.get('/api/blog/posts?fields=title,created_at' + limitParam)
+        .success(function(data){
 
-        c.posts = data;
+          c.posts = data;
 
-      });
+        });
 
-}]);
+  }]);
